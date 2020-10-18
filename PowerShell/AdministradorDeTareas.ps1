7dea12dec52a27d68201b77a911286e1161052ad
root <root@localhost.localdomain>
2020-10-17 23:10:21 -0500

    Practica 4

---
+++ PowerShell/AdministradorDeTareas.ps1
@@ -0,0 +1,35 @@
+﻿
+function Procesos{
+  $i=1
+while($i-eq 1){ 
+$opc = Read-Host -Prompt "Que deseas hacer? [1]Ver Procesos  [2]Iniciar una aplicacion  [3]Cerrar una aplicacion [4]Salir"  
+  switch($opc){
+     1 {
+     $Proceso = Read-Host -Prompt "[1]Ver todos los procesos [2]Ver un proceso en especifico" 
+      switch($Proceso){
+        1{
+          Get-Process
+        } 2 {
+          $programa = Read-Host -Prompt "Ingresa el programa"
+          Get-Process -name $programa 
+        } default{
+          " No es una opcion valida"
+        }
+      }  
+     } 2 {
+        $programa = Read-Host -Prompt "Ingresa el programa a iniciar"
+        Start-Process $programa
+        Write-Host "Programa iniciado!!" 
+     } 3 {
+        $programa = Read-Host -Prompt "Ingresa el programa que quieras cerrar"
+        Stop-Process -name $programa
+        Write-Host "Programa Finalizado!!"
+     } 4 {
+       $i = $i + 1
+     }default{
+        Write-Host "no es una opcion valida" 
+     }     
+   }  
+ }    
+}
+ Procesos
---
+++ PowerShell/EscanerDePuertos.ps1
@@ -0,0 +1,28 @@
+﻿function ScanRed{
+
+$DireccionIP = Read-Host -prompt "Ingresa el host"
+$Port = Read-Host -prompt "Ingresa el puerto a escanear"
+$Rango = (1..254)
+
+foreach ($i in $Rango){ 
+
+$ip = “{0}.{1}” –F $host,$i
+
+Write-Progress “Escaneando” $ip -PercentComplete (($i/$Rango.Count)*100)
+
+If(Test-Connection –BufferSize 32 –Count 1 –quiet –ComputerName $ip){ 
+   $socket = new-object System.Net.Sockets.TcpClient($ip, $Port)
+
+   If($socket.Connected) { 
+   Write-Host “$ip port $Port abierto”
+   $socket.Close() 
+   }
+             
+} else { 
+     Write-Host “$ip port $Port cerrado ” 
+  }
+
+ }
+}
+
+scanRed
